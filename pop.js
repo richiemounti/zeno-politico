7be0dfbf3f0d3256736aacc1d8ec4b6929ee2402
richiemounti <babtdaddy67@gmail.com>
2019-01-29 13:25:50 +0300

    add pop.js

---
+++ pop.js
@@ -0,0 +1,11 @@
+/* Side nav width set to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+/* Set the width of the sidenav to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
